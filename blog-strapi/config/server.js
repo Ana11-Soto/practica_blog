3b0aa7da5fc4b080b10a60f3ef38fdc41141f974
Ana11-Soto <analaura.18soto@gmail.com>
2020-12-01 09:10:31 -0600

    blog-strapi-gatsby

---
+++ blog-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21f8d0d96224c735ebe333c031e23edd'),
+    },
+  },
+});
---
+++ blog-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5125fa5d-b2d0-442a-9a9b-be1ec1dd7aef'
+};
